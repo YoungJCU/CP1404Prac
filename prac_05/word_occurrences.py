cfded1eabefce7f16947dffdc1cd7aa9735d50d7
chongyuang <yuang.chong@my.jcu.edu.au>
2025-06-22 18:11:52 +0800

    test code can work

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,4 +3,10 @@ estimate: 30
 actual:
 """
 text=input("Text: ")
+words=text.split()
+word_to_count = {}
+for word in words:
+    word_to_count[word] = word_to_count.get(word, 0)+1
+
+print(word_to_count)
 word_to_count={}
