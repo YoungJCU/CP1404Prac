0a600c9031c11802195f06ecbf802bf199993ca8
chongyuang <yuang.chong@my.jcu.edu.au>
2025-06-22 19:19:45 +0800

    divide different situation and print name and email

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,12 +1,26 @@
 """
 estimate:60
-actual:
+actual:48
 """
 
-email=input("Email: ").strip()
-name_split=email.split('@')[0]
-name_clean=name_split.replace('.',' ')
-names=name_clean.title()
-print(names)
+
+email_to_name = {}
+
+email = input("Email: ").strip()
 
 while email != "":
+    name_split = email.split('@')[0]
+    name_clean = name_split.replace('.', ' ')
+    name = name_clean.title()
+
+    name_check = input(f"Is your name {name}? (Y/n) ").strip().lower()
+
+    if name_check not in ("", "y"):
+        name = input("Name: ")
+
+    email_to_name[email] = name
+
+    email = input("Email: ").strip()
+
+for email, name in email_to_name.items():
+    print(f"{name} ({email})")
