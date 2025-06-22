fa7657293b11527d90f64300b3611db48dc17427
chongyuang <yuang.chong@my.jcu.edu.au>
2025-06-22 18:27:47 +0800

    sort and check mistake

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,11 +2,14 @@
 estimate: 30
 actual:
 """
+
 text=input("Text: ")
 words=text.split()
+
 word_to_count = {}
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0)+1
 
-print(word_to_count)
-word_to_count={}
+
+for word in sorted(word_to_count.keys()):
+    print(f"{word}: {word_to_count[word]}")
