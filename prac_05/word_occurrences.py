4487d73152da9523d5b3881de0f3390974568ea3
chongyuang <yuang.chong@my.jcu.edu.au>
2025-06-22 18:30:50 +0800

    add len check

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -10,6 +10,8 @@ word_to_count = {}
 for word in words:
     word_to_count[word] = word_to_count.get(word, 0)+1
 
+word_width=max(len(word) for word in word_to_count)
+
 
 for word in sorted(word_to_count.keys()):
-    print(f"{word}: {word_to_count[word]}")
+    print(f"{word:{word_width+1}}: {word_to_count[word]}")
