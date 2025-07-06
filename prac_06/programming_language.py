6e7eb4b364bd75c5165be95a8172e8faa31a0dab
chongyuang <yuang.chong@my.jcu.edu.au>
2025-07-06 20:22:59 +0800

    return string like the example

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -12,3 +12,5 @@ class ProgrammingLanguage:
 
     def is_dynamic(self):
         return self.typing=="Dynamic"
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
