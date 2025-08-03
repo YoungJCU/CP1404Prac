ae1d5ec14bfb0f870fdf52cd4536bd89e34956fa
chongyuang <yuang.chong@my.jcu.edu.au>
2025-08-03 20:45:06 +0800

    import wiki

---
+++ prac_10/wiki.py
@@ -0,0 +1,7 @@
+import wikipedia
+
+wikipedia.search("Barack")
+[u'Barak (given name)', u'Barack Obama', u'Barack (brandy)', u'Presidency of Barack Obama', u'Family of Barack Obama', u'First inauguration of Barack Obama', u'Barack Obama presidential campaign, 2008', u'Barack Obama, Sr.', u'Barack Obama citizenship conspiracy theories', u'Presidential transition of Barack Obama']
+
+wikipedia.suggest("Barak Obama")
+u'Barack Obama'
