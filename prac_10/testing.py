832a0a18041920d5d8200f2814dfd1faf4415dad
chongyuang <yuang.chong@my.jcu.edu.au>
2025-08-03 20:27:11 +0800

    fix longword problem

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -4,12 +4,14 @@ Testing code using assert and doctest
 """
 
 import doctest
-from prac_09.car import Car
+from prac_06.car import Car
 
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return "".join([s]*n)
+
+
+    return " ".join([s] * n)
 
 
 def is_long_word(word, length=5):
@@ -22,7 +24,21 @@ def is_long_word(word, length=5):
     >>> is_long_word("Python", 6)
     True
     """
-    return len(word) > length
+    return len(word) >= length
+def format_sentence(phrase):
+    """
+    Format a phrase as a sentence: capitalize first letter, end with a full stop.
+
+    >>> format_sentence("hello")
+    'Hello.'
+    >>> format_sentence("It is an ex parrot.")
+    'It is an ex parrot.'
+    >>> format_sentence("what a nice day")
+    'What a nice day.'
+    """
+    phrase = phrase.strip().rstrip('.')
+    return phrase[0].upper() + phrase[1:] + '.'
+
 
 
 def run_tests():
